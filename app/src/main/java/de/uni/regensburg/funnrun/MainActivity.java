355ad3c52a0aace677aa282f3c51f19f1953384a
primafox <87973272+primafox@users.noreply.github.com>
2021-07-27 12:02:56 +0200

    Initial commit

---
+++ app/src/main/java/de/uni/regensburg/funnrun/MainActivity.java
@@ -0,0 +1,14 @@
+package de.uni.regensburg.funnrun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
